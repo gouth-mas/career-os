066fc5683f2bba17a320614be062045a267051e9
Goutham V <goutham@Gouthams-MacBook-Air.local>
2026-06-09 19:02:30 +1000

    Add all 12 target firms to career page monitor

--- monitor.py
+++ monitor.py
@@ -7,9 +7,16 @@ from datetime import datetime
 pages = {
     "Macquarie": "https://www.macquarie.com/au/en/careers/early-careers.html",
     "JPMorgan": "https://careers.jpmorgan.com/au/en/students/programs",
+    "Morgan Stanley": "https://www.morganstanley.com/people/students-graduates",
+    "Goldman Sachs": "https://www.goldmansachs.com/careers/students/",
+    "CBA": "https://www.commbank.com.au/about-us/careers/graduates.html",
+    "NAB": "https://www.nab.com.au/about-us/careers/early-careers",
+    "Westpac": "https://www.westpac.com.au/about-westpac/working-with-us/early-careers/",
     "Deloitte": "https://www2.deloitte.com/au/en/careers/students-graduates.html",
     "KPMG": "https://www.kpmg.com.au/careers/students-graduates/",
     "PwC": "https://www.pwc.com.au/careers/student-and-graduate.html",
+    "EY": "https://www.ey.com/en_au/careers/students",
+    "Grant Thornton": "https://www.grantthornton.com.au/careers/students-and-graduates/",
 }
 
 current = {}
